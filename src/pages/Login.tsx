4c96f21194e665f12bc3d52cc01e184d5aff6c16
Morgan <morgan.espindola@gmail.com>
2025-05-04 19:50:43 -0300

    created Login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,43 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../services/firebase";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/campeonatos");
+    } catch (err: any) {
+      setError("Login inválido");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Senha"
+        />
+        <button type="submit">Entrar</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
