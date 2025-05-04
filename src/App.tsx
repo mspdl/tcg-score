d46b1d2f6041fa59af595909a35a9905ae36c573
Morgan <morgan.espindola@gmail.com>
2025-05-04 20:06:27 -0300

    updated app to navegate between routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import { JSX } from "react/jsx-runtime";
+import Championships from "./pages/Championships";
+import ChampionshipView from "./pages/ChampionshipView";
+import Games from "./pages/Games";
+import Login from "./pages/Login";
+import Players from "./pages/Players";
 import { auth } from "./services/firebase";
 
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) return <p>Carregando...</p>;
+  if (!user) return <Navigate to="/login" replace />;
+  return children;
+};
+
 function App() {
-  useEffect(() => {
-    const testLogin = async () => {
-      try {
-        const userCredential = await signInWithEmailAndPassword(
-          auth,
-          "email@exemplo.com", // coloque um e-mail real
-          "senha123" // coloque a senha correspondente
-        );
-        console.log("Login bem-sucedido:", userCredential.user);
-      } catch (error) {
-        console.error("Erro ao fazer login:", error);
-      }
-    };
-
-    testLogin();
-  }, []);
-
-  return <div>Testando login... veja o console</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/campeonato/:id" element={<ChampionshipView />} />{" "}
+        {/* Pública */}
+        <Route
+          path="/campeonatos"
+          element={
+            <ProtectedRoute>
+              <Championships />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/jogos"
+          element={
+            <ProtectedRoute>
+              <Games />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/jogadores"
+          element={
+            <ProtectedRoute>
+              <Players />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/campeonatos" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
